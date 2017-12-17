360de89cfe0c956528f0eea5fff5b37354b09f27
stephencarter.io <stephen@stephencarter.io>
2017-12-17 16:03:36 +1100

    Complete Solution

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumberWizard : MonoBehaviour {
+
+	int max;
+	int min;
+	int guess;
+
+	public int maxGuessesAllowed = 5;
+	public Text text;
+
+	// Use this for initialization
+	void Start () {
+		StartGame ();
+	}
+
+	void StartGame() {
+		max = 1001;
+		min = 1;
+		guess = NextGuess ();
+	}
+
+	public void GuessHigher() {
+		min = guess;
+		NextGuess ();
+	}
+
+	public void GuessLower() {
+		max = guess;
+		NextGuess ();
+	}
+
+	void NextGuess() {
+		guess = Random.Range (min, max + 1);
+		text.text = guess.ToString ();
+		maxGuessesAllowed = maxGuessesAllowed - 1;
+		if (maxGuessesAllowed <= 0) {
+			Application.LoadLevel ("Win");
+		}
+	}
+
+}
